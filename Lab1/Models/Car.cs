78175c8bbe14fc4c42a75f0d8c92e97eac3e948e
Jörgen Nihlgård <j.nihlgard@telia.com>
2016-04-19 13:46:58 +0200

    2

---
+++ Lab1/Models/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab1.Models
+{
+    public class Car
+    {
+        public int CarID { get; set; }
+        public string Brand { get; set; }
+    }
+}
